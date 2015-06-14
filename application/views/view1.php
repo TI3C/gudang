374edb466fd115287de2ca87c8dc74f43e8cc138
ivenulutr <ivenulutrizki@gmail.com>
2015-06-14 11:09:31 +0700

    Ditambahkan kelas

--- application/views/view1.php
+++ application/views/view1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	// Jobdesc oleh Ivenulut Rizki
+	// Jobdesc oleh Ivenulut Rizki TI3C
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
